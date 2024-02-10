12778cf1fb040eeed5491aad2c36e935142c5597
RosemaryChung <153201327+RosemaryChung@users.noreply.github.com>
2024-02-10 23:23:56 +0200

    Update IntroToVariables.java

--- SimplePractice.java
+++ SimplePractice.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package simplepractice;
 
 /**
@@ -10,11 +7,23 @@ package simplepractice;
  */
 public class SimplePractice {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+             //run the code then try these for yourself with different names for the variables
+        int cat=4;            //"int cat" means there is an integer called cat
+        int corn=5;            //same thing as above^ applies here
+        System.out.println(cat+corn);        //the code outputs the sum of the two numbers, 4 and 5 (these numbers were declared (named) cat and corn)
+        
+        String name=JOptionPane.showInputDialog("Enter your name"); //run the code and try figure out what this means
+        System.out.println(name);
+
+        double num1=2;
+        double num2=9,44;
+        System.out.println(num1+num2); //what do you notice different about a 'double' and an 'int'
+
+        char initial=R;
+        System.out.println("My initial is: " + initial); //copy this code and try it on your name
+   
     }
     
 }
