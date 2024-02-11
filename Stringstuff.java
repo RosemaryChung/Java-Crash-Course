3253c1bdc929065588c54419f7a00e549d382614
RosemaryChung <153201327+RosemaryChung@users.noreply.github.com>
2024-02-11 11:24:01 +0200

    Add files via upload

---
+++ Stringstuff.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stringstuff;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rosy
+ */
+public class Stringstuff {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String word=JOptionPane.showInputDialog("WORDDD");
+        
+        String passw1;
+        int passw2;
+        char passw3;
+        String passw4;
+        String word2;
+        
+        
+         passw1=word.substring(0,1);
+                 passw1=passw1.toUpperCase();
+                 
+                 passw2=word.length();
+                 passw3=word.charAt(4);
+                 passw4=word.substring(passw2-3,passw2);
+                 
+                 word2=word.replaceAll("#"," " );
+                 
+                 System.out.println("Passwords=" + passw1 + passw2 + passw3 + 
+                         passw4 + "\n" + "Original word=" + word2);
+                 
+                 
+                 
+                 
+                 
+         
+        
+        
+        
+        
+    }
+    
+}
